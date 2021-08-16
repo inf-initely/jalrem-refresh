f4796e2949de3b1ed04ad3084f45e003e16455f5
RifalAl <aldi4rifaldi@gmail.com>
2021-08-16 21:49:15 +0800

    feat: tambah meta di head

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,6 +1,31 @@
 <!-- Required meta tags -->
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="title" content="Jalur Rempah Nusantara">
+<meta name="description" content="Jalur Rempah adalah jalur budaya untuk pengembangan budaya bahari & literasi maritim diinisiasi oleh Kemdikbudristek RI">
+<meta name="keywords" content="rempah, kemdikbud, kemendikbud">
+<meta name="robots" content="index, follow">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="language" content="id">
+<meta name="revisit-after" content="2 days">
+<meta name="author" content="Kemdikbudristek RI">
+
+<!-- Open Graph / Facebook -->
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://metatags.io/">
+<meta property="og:title" content="Jalur Rempah Nusantara">
+<meta property="og:description" content="Jalur Rempah adalah jalur budaya untuk pengembangan budaya bahari & literasi maritim diinisiasi oleh Kemdikbudristek RI">
+<meta property="og:image" content="{{ asset('assets/img/meta-img.jpg') }}">
+
+
+<!-- Twitter -->
+<meta property="twitter:card" content="summary_large_image">
+<meta property="twitter:url" content="https://metatags.io/">
+<meta property="twitter:title" content="Jalur Rempah Nusantara">
+<meta property="twitter:description" content="Jalur Rempah adalah jalur budaya untuk pengembangan budaya bahari & literasi maritim diinisiasi oleh Kemdikbudristek RI">
+<meta property="twitter:image" content="{{ asset('assets/img/meta-img.jpg') }}">
+
+
 <!-- Bootstrap CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <link href="https://use.fontawesome.com/releases/v5.10.2/css/all.css" rel="stylesheet">
