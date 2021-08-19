20ef93d8e9083011aa7735a4cda9d117dc82ef29
RifalAl <aldi4rifaldi@gmail.com>
2021-08-19 13:49:40 +0800

    fix: meta tag di header

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,40 +1,27 @@
-<!-- Required meta tags -->
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="title" content="Jalur Rempah Nusantara">
-<meta name="description" content="Jalur Rempah adalah jalur budaya untuk pengembangan budaya bahari & literasi maritim diinisiasi oleh Kemdikbudristek RI">
-<meta name="keywords" content="rempah, kemdikbud, kemendikbud">
-<meta name="robots" content="index, follow">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<meta name="language" content="id">
-<meta name="revisit-after" content="2 days">
-<meta name="author" content="Kemdikbudristek RI">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="title" content="Jalur Rempah Nusantara">
+    <meta name="description" content="Jalur Rempah adalah jalur budaya untuk pengembangan budaya bahari & literasi maritim diinisiasi oleh Kemdikbudristek RI">
+    <meta name="keywords" content="rempah, kemdikbud, kemendikbud">
+    <meta name="robots" content="index, follow">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="language" content="id">
+    <meta name="revisit-after" content="2 days">
+    <meta name="author" content="Kemdikbudristek RI">
 
-<!-- Open Graph / Facebook -->
-<meta property="og:type" content="website">
-<meta property="og:url" content="https://metatags.io/">
-<meta property="og:title" content="Jalur Rempah Nusantara">
-<meta property="og:description" content="Jalur Rempah adalah jalur budaya untuk pengembangan budaya bahari & literasi maritim diinisiasi oleh Kemdikbudristek RI">
-<meta property="og:image" content="{{ asset('assets/img/meta-img.jpg') }}">
-
-
-<!-- Twitter -->
-<meta property="twitter:card" content="summary_large_image">
-<meta property="twitter:url" content="https://metatags.io/">
-<meta property="twitter:title" content="Jalur Rempah Nusantara">
-<meta property="twitter:description" content="Jalur Rempah adalah jalur budaya untuk pengembangan budaya bahari & literasi maritim diinisiasi oleh Kemdikbudristek RI">
-<meta property="twitter:image" content="{{ asset('assets/img/meta-img.jpg') }}">
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://metatags.io/">
+    <meta property="og:title" content="Jalur Rempah Nusantara">
+    <meta property="og:description" content="Jalur Rempah adalah jalur budaya untuk pengembangan budaya bahari & literasi maritim diinisiasi oleh Kemdikbudristek RI">
+    <meta property="og:image" content="{{ asset('assets/img/meta-img.jpg') }}">
 
 
-<!-- Bootstrap CSS -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<link href="https://use.fontawesome.com/releases/v5.10.2/css/all.css" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/slick.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/slick-theme.css') }}">
-<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-<!-- <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/flickity/2.0.5/flickity.min.css"> -->
-<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.0.7/css/swiper.min.css">
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/hero-slider.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/jalur-rempah.css') }}">
-<title>@yield('title')</title>
-<link rel="shortcut icon" href="{{ asset('assets/img/logo/favicon.png') }}">
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://metatags.io/">
+    <meta property="twitter:title" content="Jalur Rempah Nusantara">
+    <meta property="twitter:description" content="Jalur Rempah adalah jalur budaya untuk pengembangan budaya bahari & literasi maritim diinisiasi oleh Kemdikbudristek RI">
+    <meta property="twitter:image" content="{{ asset('assets/img/meta-img.jpg') }}">
+
