5c0bb7da446c726a676887b2e59a0aab6945ee49
RifalAl <aldi4rifaldi@gmail.com>
2021-08-19 09:27:11 +0800

    fix: ubah google analytic source di halaman admin beranda

--- resources/views/admin/content/home.blade.php
+++ resources/views/admin/content/home.blade.php
@@ -13,7 +13,7 @@
      <div class="container-fluid" id="contentWrapper">
         <div class="row justify-content-center">
           <div class="col-lg-12 text-center">
-            <iframe width="100%" height="900px" src="https://datastudio.google.com/embed/reporting/b5f81044-83c9-41d7-8517-c013fc7014e3/page/6zXD" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <iframe width="100%" height="900px" src="https://datastudio.google.com/embed/reporting/631a34e1-fa02-4a8f-b188-97d45c01e3b9/page/S33B" frameborder="0" style="border:0" allowfullscreen></iframe>
           </div>
         </div>
       </div>
