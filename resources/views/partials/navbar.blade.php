1fe88a887e6f84a9ebe053b07bba8bf7d8865e04
RifalAl <aldi4rifaldi@gmail.com>
2021-08-23 13:44:08 +0800

    fix: perbaikan menu language di mobile

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -9,7 +9,7 @@
     <ul class="link-bahasa d-block d-lg-none">
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          <img src="{{ asset('assets/img/bendera/flag-indonesia.png') }}" class="mr-2 flag" width="20px"> ID
+          <img src="{{ asset('assets/img/bendera/flag-english.png') }}" class="mr-2 flag" width="20px"> EN
         </a>
         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
           <li>
