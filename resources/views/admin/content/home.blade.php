0282527adce101829890c695fcad1204f48a6529
RifalAl <aldi4rifaldi@gmail.com>
2021-08-23 19:08:58 +0800

    fix: ubah source google analytic di beranda admin dan ubah size logo di admin

--- resources/views/admin/content/home.blade.php
+++ resources/views/admin/content/home.blade.php
@@ -13,7 +13,9 @@
      <div class="container-fluid" id="contentWrapper">
         <div class="row justify-content-center">
           <div class="col-lg-12 text-center">
-            <iframe width="100%" height="900px" src="https://datastudio.google.com/embed/reporting/631a34e1-fa02-4a8f-b188-97d45c01e3b9/page/S33B" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <iframe width="100%" height="900px" src="https://datastudio.google.com/embed/reporting/405b7bb5-8698-431a-8139-8623c8cdbb5e/page/S33B" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+            <!-- https://datastudio.google.com/embed/reporting/405b7bb5-8698-431a-8139-8623c8cdbb5e/page/S33B -->
           </div>
         </div>
       </div>
