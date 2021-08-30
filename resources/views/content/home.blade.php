c443769c6f343a9e76e2866196fb7b7e93e84add
RifalAl <aldi4rifaldi@gmail.com>
2021-08-31 05:38:52 +0800

    fix: perbaikan error jquery di halaman home/beranda

--- resources/views/content/home.blade.php
+++ resources/views/content/home.blade.php
@@ -371,64 +371,6 @@
 @endsection
 
 @section('js')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
-<script>
-$(document).ready(function() {
-  if ($(window).width() <= 1000) {
-    $(".navbar").addClass("bg-nav");
-    $(".navbar").removeClass("bg-trans");
-  }
-});
-$(window).scroll(function() {
-
-  if ($(window).width() >= 1000) {
-    var scroll = $(window).scrollTop();
-    //>=, not <=
-    if (scroll >= 50) {
-      //clearHeader, not clearheader - caps H
-      $(".navbar").addClass("bg-nav");
-      $(".navbar").removeClass("bg-trans");
-    } else {
-      $(".navbar").addClass("bg-trans");
-      $(".navbar").removeClass("bg-nav");
-    }
-  } else {
-    $(".navbar").addClass("bg-nav");
-    $(".navbar").removeClass("bg-trans");
-  }
-
-}); //missing );
-</script>
-<script>
-$("#selectLokasi").select2({
-  placeholder: "Pilih Lokasi",
-  allowClear: true
-});
-
-$("#selectRempah").select2({
-  placeholder: "Pilih Jenis Rempah",
-  allowClear: true
-});
-</script>
-<script>
-$(function() {
-
-  var minimized_elements = $('p.minimize');
-
-  minimized_elements.each(function() {
-    var t = $(this).text();
-    if (t.length < 90) return;
-
-    $(this).html(
-      t.slice(0, 90) + '<span>...' +
-      '<span style="display:none;">' + t.slice(90, t.length)
-    );
-
-  });
-
-});
-</script>
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script type="text/javascript"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
--- resources/views/content_english/home.blade.php
+++ resources/views/content_english/home.blade.php
@@ -20,7 +20,7 @@
                   <div class="clearfix"></div>
                 </div>
               </div>
-              <!-- end slide-inner -->
+              <!-- end slide-inner -->x
             </div>
             <!-- end swiper-slide -->
             @endforeach
@@ -370,71 +370,15 @@
 @endsection
 
 @section('js')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
-<script>
-$(document).ready(function() {
-  if ($(window).width() <= 1000) {
-    $(".navbar").addClass("bg-nav");
-    $(".navbar").removeClass("bg-trans");
-  }
-});
-$(window).scroll(function() {
-
-  if ($(window).width() >= 1000) {
-    var scroll = $(window).scrollTop();
-    //>=, not <=
-    if (scroll >= 50) {
-      //clearHeader, not clearheader - caps H
-      $(".navbar").addClass("bg-nav");
-      $(".navbar").removeClass("bg-trans");
-    } else {
-      $(".navbar").addClass("bg-trans");
-      $(".navbar").removeClass("bg-nav");
-    }
-  } else {
-    $(".navbar").addClass("bg-nav");
-    $(".navbar").removeClass("bg-trans");
-  }
-
-}); //missing );
-</script>
-<script>
-$("#selectLokasi").select2({
-  placeholder: "Pilih Lokasi",
-  allowClear: true
-});
-
-$("#selectRempah").select2({
-  placeholder: "Pilih Jenis Rempah",
-  allowClear: true
-});
-</script>
-<script>
-$(function() {
-
-  var minimized_elements = $('p.minimize');
-
-  minimized_elements.each(function() {
-    var t = $(this).text();
-    if (t.length < 90) return;
-
-    $(this).html(
-      t.slice(0, 90) + '<span>...' +
-      '<span style="display:none;">' + t.slice(90, t.length)
-    );
-
-  });
-
-});
-</script>
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script type="text/javascript"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 <script type="text/javascript" src="assets/js/slick.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.1/js/swiper.min.js"></script>
-<script src="https://platform.twitter.com/widgets.js" charset="utf-8"></script><script>
+<script src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+<script>
+
   window.addEventListener("load", function () {
     setTimeout(otherOperation, 500);
 }, false);
