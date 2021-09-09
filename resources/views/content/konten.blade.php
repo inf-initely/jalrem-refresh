f42f6f11a8fa515a3285af402a7a05daeccc44da
RifalAl <aldi4rifaldi@gmail.com>
2021-09-09 21:52:30 +0800

    fix: perbaikan tampilan youtube halaman konten (ise tab)

--- resources/views/content/konten.blade.php
+++ resources/views/content/konten.blade.php
@@ -135,7 +135,23 @@
                       @foreach( $video as $v ) 
                       <div class="col-md-12 col-lg-4 mb-4">
                         <div class="card no-border card-artikel">
-                          <div class="ytdefer video media-video" data-alt="youtube jalur rempah" data-src="{{ $v->youtube_key }}"></div>
+                          <!-- <div class="ytdefer video media-video" data-alt="youtube jalur rempah" data-src="{{ $v->youtube_key }}"></div> -->
+                          <div class="video media-video" data-video-id="{{ $v->youtube_key }}">
+                            <!--ganti id sesuai id youtube yang akan ditampilkan-->
+                            <div class="video-layer">
+                              <div class="video-placeholder">
+                                <!-- ^ div is replaced by the YouTube video -->
+                              </div>
+                            </div>
+                            <div class="video-preview" style="background: url('https://img.youtube.com/vi/{{ $v->youtube_key }}/hqdefault.jpg') 50% 50% no-repeat; background-size: cover;">
+                              <!-- this icon would normally be implemented as a character in an icon font or svg spritesheet, or similar -->
+                              <svg viewBox="0 0 74 74">
+                                <circle style="opacity:0.64;stroke:#fff" cx="37" cy="37" r="36.5"></circle>
+                                <circle fill="none" stroke="#fff" cx="37" cy="37" r="36.5"></circle>
+                                <polygon fill="#fff" points="33,22 33,52 48,37"></polygon>
+                              </svg>
+                            </div>
+                          </div>
                           <a class="stretched-link lightbox" href="{{ route('video_detail', $v->slug) }}"></a>
                           <div class="card-body">
                             <p class="card-text">{{ $v->judul_indo }}</p>
@@ -235,7 +251,43 @@
     
     }); //missing );
     </script>
+    <script>
+      var tag = document.createElement('script');
+      tag.src = "https://www.youtube.com/iframe_api";
+      document.body.appendChild(tag);
 
+      // When the YouTube API code loads, it calls this function, so it must be global
+      // and it must be named exactly onYouTubeIframeAPIReady.
+      window.onYouTubeIframeAPIReady = function() {
+        var videoModules = document.querySelectorAll('.video');
+        // for Internet Explorer 11 and below, convert array-like NodeList to an actual Array.
+        videoModules = Array.prototype.slice.call(videoModules);
+        videoModules.forEach(initializeVideoModule);
+      }
+
+      function initializeVideoModule(videoModule) {
+        var player = new YT.Player(videoModule.querySelector('.video-placeholder'), {
+          videoId: videoModule.dataset.videoId,
+          events: {
+            onStateChange: function(event) {
+              var isEnded = event.data === YT.PlayerState.ENDED;
+              // 'playing' css class controls fading the video and preivew images in/out.
+              // Internet Explorer 11 and below do not support a second argument to `toggle`
+              // videoModule.classList.toggle('playing', !isEnded);
+              videoModule.classList[isEnded ? 'remove' : 'add']('playing');
+              // if the video is done playing, remove it and re-initialize
+              if (isEnded) {
+                player.destroy();
+                videoModule.querySelector('.video-layer').innerHTML = (
+                  '<div class="video-placeholder"></div>'
+                );
+                initializeVideoModule(videoModule);
+              }
+            }
+          }
+        });
+      }
+    </script>
     <script>
     $(function() {
     
