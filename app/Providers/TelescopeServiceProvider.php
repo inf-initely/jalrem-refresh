72897c371e277481e9846f943dfc44b4f929d87f
Arhen <itsme.arhen@gmail.com>
2021-09-27 13:24:50 +0800

    fix: add new acc to access telescope

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -64,7 +64,8 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     {
         Gate::define('viewTelescope', function ($user) {
             return in_array($user->email, [
-                'redaksijalurrempah@gmail.com'
+                'redaksijalurrempah@gmail.com',
+                'nizar@gmail.com'
             ]);
         });
     }
