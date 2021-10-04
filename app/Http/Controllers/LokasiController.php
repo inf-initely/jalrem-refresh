fb434de925dab4a79e3877ca40792567cd7dec0e
rahmathidayathm03@gmail.com <rahmathidayathm03@gmail.com>
2021-10-04 18:52:45 +0700

    fix: order by location name dropdown

--- app/Http/Controllers/LokasiController.php
+++ app/Http/Controllers/LokasiController.php
@@ -6,11 +6,18 @@ use Illuminate\Http\Request;
 
 use App\Models\Lokasi;
 
+use Session;
+
 class LokasiController extends Controller
 {
     public function getJSON()
     {
-        $lokasi = Lokasi::all();
+        if( Session::get('lg') == 'en' ) {
+            $lokasi = Lokasi::orderBy('nama_lokasi_english')->get();
+        } else {
+            $lokasi = Lokasi::orderBy('nama_lokasi')->get();
+        }
+        
         return response()->json($lokasi);
     }
 }
