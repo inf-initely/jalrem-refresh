386e09b9e57b23e78120c3ecf22b3bfa9a1eb560
Ahmad Rifaldi <aldi4rifaldi@gmail.com>
2022-02-25 16:51:33 +0800

    Fix: Hapus file youtube dan twitter di JS file
    hapus line code yang tidak terapakai di file js

--- resources/views/partials/js.blade.php
+++ resources/views/partials/js.blade.php
@@ -51,60 +51,8 @@ $(function() {
 
 });
 </script>
-<script>
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
-document.body.appendChild(tag);
 
-// When the YouTube API code loads, it calls this function, so it must be global
-// and it must be named exactly onYouTubeIframeAPIReady.
-window.onYouTubeIframeAPIReady = function() {
-  var videoModules = document.querySelectorAll('.video');
-  // for Internet Explorer 11 and below, convert array-like NodeList to an actual Array.
-  videoModules = Array.prototype.slice.call(videoModules);
-  videoModules.forEach(initializeVideoModule);
-}
 
-function initializeVideoModule(videoModule) {
-  var player = new YT.Player(videoModule.querySelector('.video-placeholder'), {
-    videoId: videoModule.dataset.videoId,
-    events: {
-      onStateChange: function(event) {
-        var isEnded = event.data === YT.PlayerState.ENDED;
-        // 'playing' css class controls fading the video and preivew images in/out.
-        // Internet Explorer 11 and below do not support a second argument to `toggle`
-        // videoModule.classList.toggle('playing', !isEnded);
-        videoModule.classList[isEnded ? 'remove' : 'add']('playing');
-        // if the video is done playing, remove it and re-initialize
-        if (isEnded) {
-          player.destroy();
-          videoModule.querySelector('.video-layer').innerHTML = (
-            '<div class="video-placeholder"></div>'
-          );
-          initializeVideoModule(videoModule);
-        }
-      }
-    }
-  });
-}
-</script>
-<script>
-var tweets = $(".tweet");
-
-$(tweets).each(function(t, tweet) {
-
-  var id = $(this).attr('id');
-
-  twttr.widgets.createTweet(
-    id, tweet, {
-      conversation: 'none', // or all
-      cards: 'hidden', // or visible
-      linkColor: '#cc0000', // default is blue
-      theme: 'light' // or dark
-    });
-
-});
-</script>
 <script>
 AOS.init({ disable: 'mobile' });
 </script>
@@ -118,46 +66,6 @@ $(document).ready(function() {
     } else {
       $("#languageFlag").attr("src", "assets/img/bendera/flag-english.png");
     }
-  });
-});
-</script>
-<script>
-$(document).ready(function() {
-  $('.youtube-video').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 2,
-    slidesToScroll: 2,
-    responsive: [{
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          arrows: false
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
   });
 });
 </script>
