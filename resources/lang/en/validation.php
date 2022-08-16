3017e1294712677e0ccfce32d1df5dbf3e35a81d
Arhen <itsme.arhen@gmail.com>
2022-08-16 17:11:01 +0800

    hotfix!: update custom message error captcha

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -122,6 +122,10 @@ return [
     'uploaded' => 'The :attribute failed to upload.',
     'url' => 'The :attribute must be a valid URL.',
     'uuid' => 'The :attribute must be a valid UUID.',
+    'captcha' => 'The :attribute is invalid.',
+    'attributes' => [
+        'captcha' => 'Captcha',
+    ],
 
     /*
     |--------------------------------------------------------------------------
