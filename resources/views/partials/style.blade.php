320ca155316c5a162c620404f141240f0bf31262
Aiman Fauzy <aimanfauzy.99@gmail.com>
2023-07-15 20:52:34 +0800

    moved style head to partials & added preloader

---
+++ resources/views/partials/style.blade.php
@@ -0,0 +1,21 @@
+<!-- Bootstrap CSS -->
+<link rel="preload" as="style" type="text/css" href="{{ asset('assets/css/bootstrap.min.css') }}">
+<link rel="preload" as="style" type="text/css" href="{{ asset('assets/css/jalur-rempah.css') }}?v={{ config('app.version') }}">
+<link rel="preload" as="style" type="text/css" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+    integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/jalur-rempah.css') }}?v={{ config('app.version') }}">
+<link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+    integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+
+{{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="preload" as="style"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> --}}
+{{--
+<link href="{{ asset('assets/css/fontawesome.css') }}" rel="stylesheet"> --}}
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/slick.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/slick-theme.css') }}">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.0.7/css/swiper.min.css">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/hero-slider.css') }}?v={{ config('app.version') }}">
