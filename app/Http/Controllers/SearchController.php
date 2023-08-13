6642c87041751b05cf0076bf098512405d7053e5
Aiman Fauzy <aimanfauzy.99@gmail.com>
2023-08-13 11:42:26 +0800

    refactor: allow search controller fields reuse in other controllers

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -22,12 +22,32 @@ use App\Models\Kerjasama;
 
 class SearchController extends Controller
 {
-    public static function translatedFields() {
+    public static $THUMBNAIL = 0;
+    public static $CLOUD_KEY = 1;
+    public static $YOUTUBE_KEY = 2;
+
+    public static function contentFields(string $type, string $tableName, int $nonNullField) {
         return [
+            DB::raw($nonNullField === SearchController::$THUMBNAIL ? "thumbnail" : "NULL as thumbnail"),
+            DB::raw($nonNullField === SearchController::$CLOUD_KEY ? "cloud_key" : "NULL as cloud_key"),
+            DB::raw($nonNullField === SearchController::$YOUTUBE_KEY ? "youtube_key" : "NULL as youtube_key"),
+            DB::raw("'$type' as content_type"),
+            DB::raw("'$tableName' as table_name"),
             "judul_indo as judul_id",
             "judul_english as judul_en",
             "slug as slug_id",
             "slug_english as slug_en",
+            ...SearchController::commonFields()
+        ];
+    }
+
+    public static function finalFields() {
+        return [
+            "thumbnail", "cloud_key", "youtube_key",
+            "content_type", "table_name",
+            "judul_id", "judul_en",
+            "slug_id", "slug_en",
+            ...SearchController::commonFields()
         ];
     }
 
@@ -37,6 +57,38 @@ class SearchController extends Controller
             "penulis",
             "id_kontributor",
             "published_at"
+        ];
+    }
+
+    public static function normalizePageItem($item, string $lang) {
+        $model = null;
+        $content = (array) $item;
+        switch($item->content_type) {
+            case "article": $model = new Artikel($content); break;
+            case "photo": $model = new Foto($content); break;
+            case "audio": $model = new Audio($content); break;
+            case "video": $model = new Video($content); break;
+            case "publication": $model = new Publikasi($content); break;
+            case "event": $model = new Kegiatan($content); break;
+            case "partnership": $model = new Kerjasama($content); break;
+        }
+
+        $categories = $model->kategori_show->map(function ($item) {
+            return $item->isi;
+        });
+
+        return [
+            "title" => $model->{'judul_' . $lang},
+            "thumbnail" => $model->thumbnail,
+            "cloud_key" => $model->cloud_key,
+            "youtube_key" => $model->youtube_key,
+            "categories" => $categories,
+            "slug" => $model->{'slug_' . $lang},
+            "author" => $model->penulis != "admin" ? $model->kontributor_relasi->nama : "admin",
+            "author_type" => $model->penulis,
+            "content_type" => $model->content_type,
+            "table_name" => $model->table_name,
+            "published_at" => Carbon::parse($model->published_at)->isoFormat("D MMMM Y")
         ];
     }
 
@@ -57,68 +109,28 @@ class SearchController extends Controller
         $perPage = 9;
 
         $subquery = DB::query()
-            ->select(
-                "thumbnail", "cloud_key", "youtube_key",
-                "content_type", "table_name",
-                "judul_id", "judul_en", "slug_id", "slug_en",
-                ...SearchController::commonFields()
-            )
+            ->select(SearchController::finalFields())
             ->from(
                 Artikel::getPageQuery($lang)
-                    ->select(
-                        DB::raw("thumbnail, NULL as cloud_key, NULL as youtube_key"),
-                        DB::raw("'article' as content_type, 'artikels' as table_name"),
-                        ...SearchController::translatedFields(),
-                        ...SearchController::commonFields(),
-                    )
+                    ->select(SearchController::contentFields("article", "artikels", SearchController::$THUMBNAIL))
                     ->whereRaw("artikels.".$fieldwhere." LIKE ?", ['%'.$query.'%'])
                     ->union(Foto::getPageQuery($lang)
-                        ->select(
-                            DB::raw("thumbnail, NULL as cloud_key, NULL as youtube_key"),
-                            DB::raw("'photo' as content_type, 'fotos' as table_name"),
-                            ...SearchController::translatedFields(),
-                            ...SearchController::commonFields(),
-                        )
+                        ->select(SearchController::contentFields("photo", "fotos", SearchController::$THUMBNAIL))
                         ->whereRaw("fotos.".$fieldwhere." LIKE ?", ['%'.$query.'%']))
                     ->union(Audio::getPageQuery($lang)
-                        ->select(
-                            DB::raw("NULL as thumbnail, cloud_key, NULL as youtube_key"),
-                            DB::raw("'audio' as content_type, 'audio' as table_name"),
-                            ...SearchController::translatedFields(),
-                            ...SearchController::commonFields(),
-                        )
+                        ->select(SearchController::contentFields("audio", "audio", SearchController::$CLOUD_KEY))
                         ->whereRaw("audio.".$fieldwhere." LIKE ?", ['%'.$query.'%']))
                     ->union(Video::getPageQuery($lang)
-                        ->select(
-                            DB::raw("NULL as thumbnail, NULL as cloud_key, youtube_key"),
-                            DB::raw("'video' as content_type, 'videos' as table_name"),
-                            ...SearchController::translatedFields(),
-                            ...SearchController::commonFields(),
-                        )
+                        ->select(SearchController::contentFields("video", "videos", SearchController::$YOUTUBE_KEY))
                         ->whereRaw("videos.".$fieldwhere." LIKE ?", ['%'.$query.'%']))
                     ->union(Publikasi::getPageQuery($lang)
-                        ->select(
-                            DB::raw("thumbnail, NULL as cloud_key, NULL as youtube_key"),
-                            DB::raw("'publication' as content_type, 'publikasis' as table_name"),
-                            ...SearchController::translatedFields(),
-                            ...SearchController::commonFields(),
-                        )
+                        ->select(SearchController::contentFields("publication", "publikasis", SearchController::$THUMBNAIL))
                         ->whereRaw("publikasis.".$fieldwhere." LIKE ?", ['%'.$query.'%']))
                     ->union(Kegiatan::getPageQuery($lang)
-                        ->select(
-                            DB::raw("thumbnail, NULL as cloud_key, NULL as youtube_key"),
-                            DB::raw("'event' as content_type, 'kegiatans' as table_name"),
-                            ...SearchController::translatedFields(),
-                            ...SearchController::commonFields(),
-                        )
+                        ->select(SearchController::contentFields("event", "kegiatans", SearchController::$THUMBNAIL))
                         ->whereRaw("kegiatans.".$fieldwhere." LIKE ?", ['%'.$query.'%']))
                     ->union(Kerjasama::getPageQuery($lang)
-                        ->select(
-                            DB::raw("thumbnail, NULL as cloud_key, NULL as youtube_key"),
-                            DB::raw("'partnership' as content_type, 'kerjasamas' as table_name"),
-                            ...SearchController::translatedFields(),
-                            ...SearchController::commonFields(),
-                        )
+                        ->select(SearchController::contentFields("partnership", "kerjasamas", SearchController::$THUMBNAIL))
                         ->whereRaw("kerjasamas.".$fieldwhere." LIKE ?", ['%'.$query.'%']))
             , "subquery_table")
             ->orderByDesc("published_at");
@@ -130,35 +142,7 @@ class SearchController extends Controller
             ->get();
 
         $mapped = $results->map(function ($item) use ($lang) {
-            $model = null;
-            $content = (array) $item;
-            switch($item->content_type) {
-                case "article": $model = new Artikel($content); break;
-                case "photo": $model = new Foto($content); break;
-                case "audio": $model = new Audio($content); break;
-                case "video": $model = new Video($content); break;
-                case "publication": $model = new Publikasi($content); break;
-                case "event": $model = new Kegiatan($content); break;
-                case "partnership": $model = new Kerjasama($content); break;
-            }
-
-            $categories = $model->kategori_show->map(function ($item) {
-                return $item->isi;
-            });
-
-            return [
-                "title" => $model->{'judul_' . $lang},
-                "thumbnail" => $model->thumbnail,
-                "cloud_key" => $model->cloud_key,
-                "youtube_key" => $model->youtube_key,
-                "categories" => $categories,
-                "slug" => $model->{'slug_' . $lang},
-                "author" => $model->penulis != "admin" ? $model->kontributor_relasi->nama : "admin",
-                "author_type" => $model->penulis,
-                "content_type" => $model->content_type,
-                "table_name" => $model->table_name,
-                "published_at" => Carbon::parse($model->published_at)->isoFormat("D MMMM Y")
-            ];
+            return SearchController::normalizePageItem($item, $lang);
         });
 
         $total = 0;
