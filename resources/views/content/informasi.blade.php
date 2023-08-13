de7c91bfb94aa74f8bd5833364008a0698f63441
Aiman Fauzy <aimanfauzy.99@gmail.com>
2023-08-13 11:47:42 +0800

    fix: card navi reuse and script refactor in /information

--- resources/views/content/informasi.blade.php
+++ resources/views/content/informasi.blade.php
@@ -153,114 +153,16 @@
             </div>
         </section>
         <!----------------------------------------------------------->
-        <section id="cardInfo">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-lg-11">
-                        <div class="row">
-                            <div class="col-lg-4 mb-2">
-                                <div class="card no-border card-media">
-                                    <div class="card-body row">
-                                        <div class="col-3 mt-3 text-center">
-                                            <img src="{{ asset('assets/img/icon/jalur_1.svg') }}" height="40px">
-                                        </div>
-                                        <div class="col-9 ">
-                                            <h3 class="judul-card-info">{{__("common.the_route")}}</h3>
-                                            <p class="des-card-info-id">{{__("wall.the_route_card_desc")}}</p>
-                                        </div>
-                                    </div>
-                                    <a href="{{ route('tentangjalur') }}" class="stretched-link"></a>
-                                </div>
-                            </div>
-                            <div class="col-lg-4 mb-2">
-                                <div class="card no-border card-media">
-                                    <div class="card-body row">
-                                        <div class="col-3 mt-3 text-center">
-                                            <img src="{{ asset('assets/img/icon/jejak_1.svg') }}" height="32px">
-                                        </div>
-                                        <div class="col-9 ">
-                                            <h3 class="judul-card-info">{{__("common.the_trail")}}</h3>
-                                            <p class="des-card-info-id">{{__("wall.the_trail_card_desc")}}</p>
-                                        </div>
-                                    </div>
-                                    <a href="{{ route('tentangjejak') }}" class="stretched-link"></a>
-                                </div>
-                            </div>
-                            <div class="col-lg-4 mb-2">
-                                <div class="card no-border card-media">
-                                    <div class="card-body row">
-                                        <div class="col-3 mt-3 text-center">
-                                            <img src="{{ asset('assets/img/icon/masa-depan_1.svg') }}" height="40px">
-                                        </div>
-                                        <div class="col-9 ">
-                                            <h3 class="judul-card-info">{{__("common.the_future")}}</h3>
-                                            <p class="des-card-info-id">{{__("wall.the_future_card_desc")}}</p>
-                                        </div>
-                                    </div>
-                                    <a href="{{ route('tentangmasadepan') }}" class="stretched-link"></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+        @include("partials.triad_card_navi_footer")
         </div>
     </main>
 @endsection
 
 @section('js')
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-    </script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
-        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    @include("partials.js.jquery")
+    @include("partials.js.bootstrap")
+    @include("partials.js.dynamic-navbar")
     <script type="text/javascript" src="{{ asset('assets/js/slick.min.js') }}"></script>
-    <script>
-        $(document).ready(function() {
-            if ($(window).width() <= 1000) {
-                $(".navbar").addClass("bg-nav");
-                $(".navbar").removeClass("bg-trans");
-            }
-        });
-        $(window).scroll(function() {
-
-            if ($(window).width() >= 1000) {
-                var scroll = $(window).scrollTop();
-                //>=, not <=
-                if (scroll >= 50) {
-                    //clearHeader, not clearheader - caps H
-                    $(".navbar").addClass("bg-nav");
-                    $(".navbar").removeClass("bg-trans");
-                } else {
-                    $(".navbar").addClass("bg-trans");
-                    $(".navbar").removeClass("bg-nav");
-                }
-            } else {
-                $(".navbar").addClass("bg-nav");
-                $(".navbar").removeClass("bg-trans");
-            }
-
-        }); //missing );
-    </script>
-    <script>
-        $(function() {
-
-            var minimized_elements = $('p.minimize');
-
-            minimized_elements.each(function() {
-                var t = $(this).text();
-                if (t.length < 90) return;
-
-                $(this).html(
-                    t.slice(0, 90) + '<span>...' +
-                    '<span style="display:none;">' + t.slice(90, t.length)
-                );
-
-            });
-
-        });
-    </script>
     <script>
         $('.kegiatan-sebelumnya').slick({
             dots: true,
